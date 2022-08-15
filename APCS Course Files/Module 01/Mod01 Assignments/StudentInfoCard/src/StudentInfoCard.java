e567ba9b4847398391cca9fd3d1f4fad9c28a628
Mac Lawson <macattackairman@gmail.com>
2022-08-15 12:14:33 -0400

    Add files via upload

---
+++ APCS Course Files/Module 01/Mod01 Assignments/StudentInfoCard/src/StudentInfoCard.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ * Student Contact Card
+ * {@summary} Date: 8 - 5 - 22
+ * {@summary} Purpose: To provide information about myself
+ * @author Mac Lawson
+ */
+
+public class StudentInfoCard {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("**********************************"
+        + "\n *                              *"
+        + "\n *     Student Contact Card     *"
+        + "\n *                              *"
+        + "\n**********************************"
+        );
+
+
+        // MYSELF
+        System.out.println("Name: Gordon Lawson | Preferred Name: Mac"); 
+        System.out.print("Birthday: March 1st 2007"); 
+        System.out.print("    Age: 15");      
+        System.out.println("    Grade: 10th");
+        System.out.print("\nSchool: Mead Hall Episcopal School");
+        System.out.print("    City: Aiken, SC");
+        System.out.println("\n \nCell: 540-935-6367, daytime, everyday");   
+        System.out.print("Email: mlawson07@protonmail.com \n");
+
+        //DIVIDER
+        System.out.println("    ---------");
+
+        //MOM'S INFO
+        System.out.println("\nCell: 703-209-6907, daytime, everyday\nEmail: laurenwlawson@gmail.com");
+
+        //MATH
+        System.out.print("\nCurrent Math Class:");
+        System.out.println(" Honors Pre Calc");
+
+        //EXPERIENCE
+        System.out.println("\nExperience: I have worked in many different comp sci fields.\nI have experience with both front and back end development, including servers.\nI am working on a cryptocurrency platform currently. ");
+
+
+        //APCS
+        System.out.println("\nI am taking APCS because I want \nto take a further dive into the\nJava language. I would like to\nlearn the mirco details of Java because\nit would allow me to gain a key understanding\nof computing.    ");
+      
+        
+    }
+
+
+
+
+
+
+    
+
+}
